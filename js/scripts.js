e7c3d9cca406e1d6f64440023ae371b7f4ab50dd
Rohan Straker <rohanstraker@gmail.com>
2019-02-19 17:00:11 -0800

    add mor functions to my web page

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-  
+
+  var name = function(firstName, lastName){
+   return firstName + lastName;
+  }
+
+  var name = function(firstName, lastName){
+  return parseInt(firstName) + parseInt(lastName);
+  };
+
+  $("#name").submit(function() {
+    event.preventDefault();
+   var firstName = $("input#firstName").val();
+   var lastName = $("input#lastName").val();
+   alert(name);
 }
