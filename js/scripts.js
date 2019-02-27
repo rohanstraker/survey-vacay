84d330f6cd7f0f26e077379ec18b010bc852c8eb
Rohan Straker <rohanstraker@gmail.com>
2019-02-27 03:52:20 -0800

    added my if else statments but still broken need to hide a function

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,17 @@ $("form#name").submit(function(event) {
  var age = parseInt($("input#age").val());
  var gender = $("select#gender").val();
 
-
+ var vaca1 = LasVegas
  $(".first,lastname").text(firstLastNameInput);
  $(".wheredoyoulive").text(whereDoYouLiveInput);
- $(".age").text(age);
 
 
 
+if (gender === "Female" && age > 100){
  $("#story").show();
+} else if (gender === "Male" && age > 100){
+  $("#story2").show();
+}
 
  event.preventDefault();
   });
