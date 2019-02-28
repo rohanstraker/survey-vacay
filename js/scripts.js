0d5d51094b3732ee3785ad72f70e8289e2c8142b
Rohan Straker <rohanstraker@gmail.com>
2019-02-27 18:02:39 -0800

    added more changes to java script

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $("form#name").submit(function(event) {
  var whereDoYouLiveInput = $("input#wheredoyoulive").val();
  var age = $("input#age").val();
  var gender = $("select#gender").val();
-
+ var
  //var vaca1 = LasVegas
  $(".first-lastname").text(firstLastNameInput);
  $(".wheredoyoulive").text(whereDoYouLiveInput);
